79bdde0d5801cf619ee187976ad59117ea1cf029
Qian Yun Zhang <zhangqy@umich.edu>
2025-09-30 14:50:22 -0400

    Created slider class

--- gui.py
+++ gui.py
@@ -3,6 +3,10 @@ import pygame_gui
 from collections import deque
 import random
 
+import pygame_widgets
+from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
+
 import serial
 import serial.tools.list_ports
 import os
@@ -202,3 +206,58 @@ class OptionsUIApp:
             pygame.display.update()
 
             ###change this stuff bc created new serial_handler file^^
+    
+    class Slider:
+        def __init__(self, pos: tuple, size: tuple, initial_val: float, min: int, max: int) -> None:
+            self.pos = pos
+            self.size = size
+
+            self.slider_left_pos = self.pos[0] - (size[0] // 2)
+            self.slider_right_pos = self.pos[0] - (size[0] // 2)
+            self.slider_top_pos = self.pos[1] - (size[1] // 2)
+
+            self.min = min
+            self.max = max
+            self.initial_val = (self.slider_right_pos - self.slider_left_pos) * initial_val
+
+
+        self.container_rect = pygame.Rect(self.slider_left_pos, self.slider_top_pos, self.size[0], self.size[1])
+        self.button_rect = pygame.Rect(self.slider_left_pos + self.initial_val - 5, self.slider_top_pos, 10, self.size[1])
+
+        # label
+        self.text = UI.fonts['m'].render(str(int(self.get_value())), True, "white", None)
+        self.label_rect = self.text.get_rect(center = (self.pos[0], self.slider_top_pos - 15))
+        
+        def move_slider(self, mouse_pos):
+            pos = mouse_pos[0]
+            if pos < self.slider_left_pos:
+                pos = self.slider_left_pos
+            if pos > self.slider_right_pos:
+                pos = self.slider_right_pos
+            self.button_rect.centerx = pos
+
+        def hover(self):
+            self.hovered = True
+
+        def render(self, app):
+            pygame.draw.rect(app.screen, "darkgray", self.container_rect)
+            pygame.draw.rect(app.screen, BUTTONSTATES[self.hovered], self.button_rect)
+        
+        def get_value(self):
+            val_range = self.slider_right_pos - self.slider_left_pos - 1
+            button_val = self.button_rect.centerx - self.slider_left_pos
+
+            return (button_val/val_range)*(self.max-self.min)+self.min
+        
+        def display_value(self, app):
+            self.text = UI.fonts['m'].render(str(int(self.get_value())), True, "white", None)
+            app.screen.blit(self.text, self.label_rect)
+
+
+
+
+
+
+
+
+
